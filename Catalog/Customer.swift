6310d63e0271e90521a5b0cf9940fbc1296eecdb
Pedro Solís García <solisgpedro@gmail.com>
2018-06-26 22:00:53 +0200

    Fixed issue with customer file

--- Catalog/Customer.swift
+++ Catalog/Customer.swift
@@ -1,9 +1,19 @@
-//
-//  Customer.swift
-//  Catalog
-//
-//  Created by Pedro Solís García on 26/06/2018.
-//  Copyright © 2018 AppCoda. All rights reserved.
-//
-
 import Foundation
+
+struct Customer: Codable {
+    let shopId: String
+    let name: String
+    let surname: String
+    let region: String
+    let dateOfWedding: String
+    var dressesNames: String
+    
+    init(shopId: String, name: String, surname: String, region: String, dateOfWedding: String, dressesNames: String){
+        self.shopId = shopId
+        self.name = name
+        self.surname = surname
+        self.region = region
+        self.dateOfWedding = dateOfWedding
+        self.dressesNames = dressesNames
+    }
+}
