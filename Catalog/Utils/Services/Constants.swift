abeec9d71c8f91ced98b6589078f2f3a75e8b566
Pedro Solís García <solisgpedro@gmail.com>
2020-06-30 19:28:14 +0200

    Update Constants.swift

--- Catalog/Utils/Services/Constants.swift
+++ Catalog/Utils/Services/Constants.swift
@@ -1,6 +1,6 @@
 public enum API: String {
   case SCHEME = "https"
-  case HOST = "8e08w9h4m5.execute-api.eu-west-1.amazonaws.com"
+  case HOST = "host.server.com" // Not prod Host. Please request
   case CUSTOMER_PATH = "/production/customers"
   case COLLECTION_PATH = "/production/v1/collections/latest"
 }
