4b60fd12c850b6668f8c5cbf9701221fa57f9fbe
Pedro <solisgpedro@gmail.com>
2020-07-02 23:45:15 +0200

    Add proper URL

--- Catalog/Utils/Services/Constants.swift
+++ Catalog/Utils/Services/Constants.swift
@@ -1,6 +1,6 @@
 public enum API: String {
   case SCHEME = "https"
-  case HOST = "host.server.com" // Not prod Host. Please request
+  case HOST = "8e08w9h4m5.execute-api.eu-west-1.amazonaws.com"
   case PATH_CUSTOMER = "/production/customers"
   case PATH_COLLECTION = "/production/v1/collections/latest"
   case PATH_SHOPID = "/production/v1/shops/availability"
