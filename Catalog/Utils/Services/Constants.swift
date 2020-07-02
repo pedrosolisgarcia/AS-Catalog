460dbb271d1a1e52922d1c1b1d07211422f71a35
Pedro <solisgpedro@gmail.com>
2020-07-02 23:48:37 +0200

    Apply gitignore

--- Catalog/Utils/Services/Constants.swift
+++
@@ -1,7 +0,0 @@
-public enum API: String {
-  case SCHEME = "https"
-  case HOST = "8e08w9h4m5.execute-api.eu-west-1.amazonaws.com"
-  case PATH_CUSTOMER = "/production/customers"
-  case PATH_COLLECTION = "/production/v1/collections/latest"
-  case PATH_SHOPID = "/production/v1/shops/availability"
-}
